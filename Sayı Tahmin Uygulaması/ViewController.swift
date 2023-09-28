9765f217805ddec39effc44a55b7ed6d4df630d4
Kadir Sever <sever.kadir@outlook.com>
2023-09-28 21:10:51 +0300

    Initial Commit

---
+++ Sayı Tahmin Uygulaması/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sayı Tahmin Uygulaması
+//
+//  Created by Kadir on 28.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
