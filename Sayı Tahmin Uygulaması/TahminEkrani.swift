03cf007f0fa902a1f2a8cf910e6658cf41f18477
Kadir Sever <sever.kadir@outlook.com>
2023-09-28 22:08:18 +0300

    updated file

---
+++ Sayı Tahmin Uygulaması/TahminEkrani.swift
@@ -0,0 +1,29 @@
+//
+//  TahminEkrani.swift
+//  Sayı Tahmin Uygulaması
+//
+//  Created by Kadir on 28.09.2023.
+//
+
+import UIKit
+
+class TahminEkrani: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
